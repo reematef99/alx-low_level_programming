43144e618a1be9bff0ff6eefcc7f5735528f93af
reematef99 <reematef677@gmail.com>
2023-08-16 09:07:04 -0700

    3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,12 @@
  *Return: always 0 (success)
 */
 void print_alphabet_x10(void)
-{	
+{
 	int i;
 	int j;
+
 	for (j = 0; i <= 10; i++)
-	{	
+	{
 		for (j = 'a'; j <= 'z'; j++)
 		_putchar(j);
 	_putchar('\n');
