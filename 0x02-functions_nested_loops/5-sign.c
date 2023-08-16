091f2cc0425f509b7d7a5846950cd99d4e7386b9
reematef99 <reematef677@gmail.com>
2023-08-16 09:48:47 -0700

    6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+  *prints_sign_determines if the input number
+  *greater, iqual or less than zero.
+  *
+  *@n: the input number as integer.
+  *
+  *return: 1 is greater than zero. 0 is zero.
+  * -1 is less than zero.
+  */
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
