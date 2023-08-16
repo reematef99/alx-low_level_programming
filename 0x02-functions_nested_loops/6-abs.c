905fe22a245a3696a6b26541075e88f9df21c4ad
reematef99 <reematef677@gmail.com>
2023-08-16 14:50:16 -0700

    7

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,13 +5,11 @@
 * of an integer.
 *
 * @i: input number as an integer.
-<<<<<<< HEAD
 *
 * Return: absolute value
-=======
 * Return:absolute value
->>>>>>> 47e94a5fa73acdc84408db1c5e4546e24af1544c
 */
+
 int _abs(int i)
 {
 	if (i >= 0)
@@ -19,14 +17,7 @@ int _abs(int i)
 		return (1);
 	}
 	else
-<<<<<<< HEAD
 	{
 		return (i * -1);
 	}
 }
-=======
- 	{
-		return (i * -1);
-	}
-}
->>>>>>> 47e94a5fa73acdc84408db1c5e4546e24af1544c
